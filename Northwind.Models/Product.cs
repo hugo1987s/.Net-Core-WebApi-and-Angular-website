4bd2d2214a45416f5f6cd82f480de04812210e51
hugo1987s <hugo.schreiber@hasar.com>
2020-02-03 23:56:47 -0300

    Agregar archivos de proyecto.

---
+++ Northwind.Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Northwind.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int SupplierId { get; set; }
+        public decimal? UnitPrice{ get; set; }
+        public string Package { get; set; }
+        public bool IsDiscontinued { get; set; }
+
+    }
+}
