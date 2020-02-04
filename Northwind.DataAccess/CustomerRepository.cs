5e95eebcf69156a37e974cac6e4d9fae9a6a5b12
hugo1987s <hugo.schreiber@hasar.com>
2020-02-04 14:35:31 -0300

    Se crean Repositories para la entidad Customer.

---
+++ Northwind.DataAccess/CustomerRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Northwind.Models;
+using Northwind.Repositories;
+
+namespace Northwind.DataAccess
+{
+    public class CustomerRepository: Repository<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(string connectionString) : base(connectionString)
+        {
+
+        }
+
+        public IEnumerable<Customer> CustomerPagedList(int page, int rows)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@page", page);
+            parameters.Add("@rows", rows);
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return connection.Query<Customer>("dbo.CustomerPagedList", parameters, commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+    }
+}
---
+++ Northwind.Repositories/ICustomerRepository.cs
@@ -0,0 +1,10 @@
+﻿using Northwind.Models;
+using System.Collections.Generic;
+
+namespace Northwind.Repositories
+{
+    public interface ICustomerRepository:IRepository<Customer>
+    {
+        IEnumerable<Customer> CustomerPagedList(int page, int rows);
+    }
+}
