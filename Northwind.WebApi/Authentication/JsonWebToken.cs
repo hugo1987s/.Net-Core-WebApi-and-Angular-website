b610fa4b7cab448c736077b9e68e6507daa61b63
hugo1987s <hugo.schreiber@hasar.com>
2020-02-05 15:42:25 -0300

    Logica para tokens

---
+++ Northwind.WebApi/Authentication/JsonWebToken.cs
@@ -0,0 +1,11 @@
+﻿namespace Northwind.WebApi.Authentication
+{
+    public class JsonWebToken
+    {
+        public string AccessToken { get; set; }
+        public string TokenType { get; set; } = "bearer";
+        public int ExpiresIn { get; set; }
+        public string RefreshToken { get; set; }
+
+    }
+}
