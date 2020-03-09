82e48ee212a1700ce111568952630b79b08bc3b1
hugo1987s <hugo.schreiber@hasar.com>
2020-03-09 19:14:59 -0300

    Controlador de Proveedores/Suppliers

---
+++ Northwind.WebApi/Controllers/SupplierController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
+using Northwind.UnitOfWork;
+
+namespace Northwind.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Supplier")]
+    [Authorize]
+    public class SupplierController : Controller
+    {
+        private readonly IUnitOfWork _unitOrWork;
+        public SupplierController(IUnitOfWork unitOfWork)
+        {
+            _unitOrWork = unitOfWork;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok(_unitOrWork.Supplier.GetById(id));
+        }
+
+        [HttpGet]
+        [Route("GetPaginateSupplier/{page:int}/{rows:int}")]
+        public IActionResult GetPaginatedSupplier(int page, int rows)
+        {
+            return Ok(_unitOrWork.Supplier.SupplierPagedList(page, rows));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Supplier supplier)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            return Ok(_unitOrWork.Supplier.Insert(supplier));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Supplier supplier)
+        {
+            if(ModelState.IsValid && _unitOrWork.Supplier.Update(supplier))
+            {
+                return Ok(new { Message = "The supplier was updated" });
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete([FromBody] Supplier supplier)
+        {
+            if (supplier.Id > 0)
+                return Ok(_unitOrWork.Supplier.Delete(supplier));
+            return BadRequest();
+        }
+
+
+
+
+    }
+}
